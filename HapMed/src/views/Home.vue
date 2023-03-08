9572f1bd4813edfc87c7d851e7c5f2f993bb4196
Wallyson Gabriel <98840067+Wallysong27@users.noreply.github.com>
2023-03-07 23:14:25 -0300

    Add files via upload

---
+++ HapMed/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Banner />
+    <div class="main-container">
+      <h1>HapMed Clinical Group</h1>
+      <ConsultaForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import Banner from '../components/Banner.vue'
+import ConsultaForm from '../components/ConsultaForm.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Banner,
+    ConsultaForm
+  }
+}
+
+</script>
